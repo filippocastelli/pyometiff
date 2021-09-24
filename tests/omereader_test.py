0af3d3a57d09049fe0a9c9ec8cbd9460a9a30b3b
Filippo Maria Castelli <filippocastelli42@gmail.com>
2021-09-24 12:39:56 +0200

    minor refactorings

--- tests/omereader_test.py
+++ tests/omereader_test.py
@@ -15,15 +15,18 @@
 
 # Copyright (c) 2021, Filippo Maria Castelli
 
-import os,sys,inspect
+import sys
+import os
+import inspect
 from pathlib import Path
 import pytest
 from mock import patch
 
 import numpy as np
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+sys.path.insert(0, parentdir)
 
 from pyometiff.omereader import OMETIFFReader
 
@@ -31,17 +34,19 @@ parentdir_path = Path(parentdir)
 test_img_path = parentdir_path.joinpath("tests/cell.ome.tiff")
 
 
-class mockedOMEXML:
-    
+class MockedOMEXML:
+
     def __init__(self, arg):
         pass
-    
+
     class Image:
         AcquisitionDate = "today"
         Name = "mock_pixels"
+
         class Pixels:
-            def __init__():
+            def __init__(self):
                 pass
+
             AcquisitionDate = "today"
             Name = "mock_pixels"
             SizeT = 10
@@ -56,90 +61,109 @@ class mockedOMEXML:
             PhysicalSizeYUnit = "µm"
             PhysicalSizeZUnit = "µm"
             DimensionOrder = "XYCZT"
-            
+
             class Channel:
                 def __init__(self, int_arg):
                     pass
+
                 Name = "channel_name"
+
     class Instrument:
         def __init__(self):
             pass
-        def get_ID(self):
+
+        @staticmethod
+        def get_ID():
             return "instrument_ID"
-    
+
         class Objective:
             def __init__(self):
                 pass
+
+            @staticmethod
             def get_ID():
                 return "objective_ID"
+
+            @staticmethod
             def get_LensNA():
                 return "lens_NA"
+
+            @staticmethod
             def get_NominalMagnification():
                 return "nominal_magnification"
-            
+
         class Microscope:
             def __init__(self):
                 pass
+
+            @staticmethod
             def get_Type():
                 return "mocked_microscope"
-            
+
         class Detector:
             def __init__(self):
                 pass
-            
+
+            @staticmethod
             def get_Model():
                 return "detector_model"
-            
+
+            @staticmethod
             def get_ID():
                 return "detector_ID"
-            
+
+            @staticmethod
             def get_Type():
-                return "mocked_detector"     
-            
+                return "mocked_detector"
+
     def image(self, imageseries=0):
         return self.Image()
-    def instrument(self,imageseries=0):
+
+    def instrument(self, imageseries=0):
         return self.Instrument()
-        
-    def get_image_count(self):
+
+    @staticmethod
+    def get_image_count():
         return 1
 
 
 class TestOMETIFFReader:
     @pytest.fixture
     def read_fixture(self, **kwargs):
-        def _read_fixture(**kwargs):
+        def _read_fixture(**_kwargs):
             reader = OMETIFFReader(**kwargs)
             reader.read()
             return reader.array, reader.metadata, reader.omexml_string
+
         return _read_fixture
-    
-    def mock_OMEXML(self):
+
+    @staticmethod
+    def mock_OMEXML():
         return 0
-    
+
     def test_init(self) -> None:
-        reader = OMETIFFReader(fpath = test_img_path)
+        reader = OMETIFFReader(fpath=test_img_path)
         assert reader.fpath == test_img_path
         assert reader.imageseries == 0
-        
+
     def test_read(self) -> None:
         reader = OMETIFFReader(fpath=test_img_path)
         array, metadata, omexml_string = reader.read()
         # pudb.set_trace()
         assert type(array) == np.ndarray
         assert len(metadata.keys()) > 0
-      
+
     def test_open_tiff(self) -> None:
         array, omexml_string = OMETIFFReader._open_tiff(fpath=test_img_path)
         assert array.shape == (10, 15, 2, 256, 256)
         assert omexml_string != ""
-    
+
     def test_with_fixture(self, read_fixture) -> None:
         array, metadata, omexml_string = read_fixture(fpath=test_img_path)
-        
-    @patch('pyometiff.omereader.OMEXML', side_effect=mockedOMEXML)
+
+    @patch('pyometiff.omereader.OMEXML', side_effect=MockedOMEXML)
     def test_parse_metadata(self, mock_object):
         # mock_omexml.get.side_effect = 1
         reader = OMETIFFReader(fpath=test_img_path)
         # pudb.set_trace()
-        array, metadata, omexml_string = reader.read()
+        _, _, _ = reader.read()
